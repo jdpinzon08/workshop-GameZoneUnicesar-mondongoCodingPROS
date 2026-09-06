2815b49f37f033be600ff80311d6ff29ee5668c5
Juan Diego Pabon <jdiegopabon@unicesar.edu.co>
2026-09-06 14:19:04 -0500

    feat: add Person abstract class with core user attributes

--- src/main/java/com/gamezone/model/Person.java
+++ src/main/java/com/gamezone/model/Person.java
@@ -1,4 +1,23 @@
 package com.gamezone.model;
 
-public class Person {
+public abstract class Person {
+    private String id;
+    private String name;
+    private String phoneNumber;
+
+    public Person(String id, String name, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
 }
